5e7ddf4b7edd958994a0594bce70b4c190f552fb
Bruno Varela <brunohilariovarela@hotmail.com>
2025-11-02 12:04:41 +0100

    create table categories

---
+++ lib/ai_email_sorting_web/live/category_live/index.ex
@@ -0,0 +1,8 @@
+defmodule AiEmailSortingWeb.CategoryLive.Index do
+  use AiEmailSortingWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+
+end
---
+++ test/support/fixtures/categories_fixtures.ex
@@ -0,0 +1,21 @@
+defmodule AiEmailSorting.CategoriesFixtures do
+  @moduledoc """
+  This module defines test helpers for creating
+  entities via the `AiEmailSorting.Categories` context.
+  """
+
+  @doc """
+  Generate a category.
+  """
+  def category_fixture(attrs \\ %{}) do
+    {:ok, category} =
+      attrs
+      |> Enum.into(%{
+        description: "some description",
+        name: "some name"
+      })
+      |> AiEmailSorting.Categories.create_category()
+
+    category
+  end
+end
